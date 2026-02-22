947fb44b1fece9b07016c22cf761bbd1fce1c4f0
orel <235223682+orel949@users.noreply.github.com>
2026-02-22 10:43:41 +0100

    fix: correction des doublons dans todayTasks

--- src/verifDate.js
+++ src/verifDate.js
@@ -1,14 +1,21 @@
-import { sauvegarderTaches, todayTasks } from "./index.js";
+import { sauvegarderTaches, todayTasks, homeTasks } from "./index.js";
 
 
 function verifierTacheJour()
 {
     const today = new Date().toISOString().split("T")[0];
-    for (let i=0;i<todayTasks.length;i++)
+    for (let i = todayTasks.length - 1; i >= 0; i--)
     {
-        if (!(todayTasks[i].date===today))
+        if (!(todayTasks[i].date === today))
         {
-            todayTasks.splice(i,1);
+            todayTasks.splice(i, 1);
+        }
+    }
+    for (let i = 0; i < homeTasks.length; i++)
+    {
+        if (homeTasks[i].date === today && !todayTasks.some(t => t.id === homeTasks[i].id))
+        {
+            todayTasks.push(homeTasks[i]);
         }
     }
     sauvegarderTaches();
